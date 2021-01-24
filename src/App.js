41fad06e39d2ca6e7e5c6496f0996ac6d048f7a7
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-24 23:13:45 +0530

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './app/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Linkedin</h1>
+      <Header />
     </div>
   );
 }
