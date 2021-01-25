6f686bd9ef5ed2d6d47f60ea3f3a361b4819aa2b
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-25 14:01:33 +0530

    Create Post cards

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create'
 import InputOption from './InputOption'
@@ -6,8 +6,15 @@ import ImageIcon from '@material-ui/icons/Image'
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
 import EventNoteIcon from '@material-ui/icons/EventNote'
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
+import Post from './Post'
 
 function Feed() {
+    const[posts, setPosts] = useState([])
+    
+    const sendPost = e => {
+        e.preventDefault()
+    }
+
     return(
         <div className='feed'>
             <div className='feed__inputContainer'>
@@ -15,7 +22,7 @@ function Feed() {
                     <CreateIcon />
                     <form>
                         <input type='text' />
-                        <button type='submit'>Send</button>
+                        <button onClick={sendPost} type='submit'>Send</button>
                     </form>
                 </div>
 
@@ -26,6 +33,15 @@ function Feed() {
                     <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7FC15E' />
                 </div>
             </div>
+
+            {posts.map((post) => (
+                <Post />
+            ))}
+            <Post 
+                name='Subhankar Raj' 
+                description='This is a test'
+                message='Wow  this worked'
+            />
         </div>
     )
 }
